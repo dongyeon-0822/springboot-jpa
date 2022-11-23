d0c66820b769e2340a7128c0c3ac62979cbc91e8
DongYeon Kang <myjjue00@gmail.com>
2022-11-23 15:19:36 +0900

    feat: publisher 추가

--- src/main/java/com/example/springbootjpa/domain/dto/BookResponse.java
+++ src/main/java/com/example/springbootjpa/domain/dto/BookResponse.java
@@ -14,12 +14,16 @@ public class BookResponse {
     private Long bookId;
     private String bookName;
     private String authorName;
+    private String publisherName;
+
 
     public static BookResponse of(Book book) { //매개변수로 book과 작가명을 받아 Response 생성
         return BookResponse.builder()
                 .bookId(book.getId())
                 .bookName(book.getName())
                 .authorName(book.getAuthor().getName())
+                .publisherName(book.getPublisher().getName())
                 .build();
     }
+
 }
