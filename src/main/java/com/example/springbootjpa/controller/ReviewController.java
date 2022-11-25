c36ae63f86fcc72578ab3ec666c47d7582f98209
DongYeon Kang <myjjue00@gmail.com>
2022-11-25 14:25:34 +0900

    feat: RequiredArgsConstructor 추가
    - 필요한 생성자 자동 생성

--- src/main/java/com/example/springbootjpa/controller/ReviewController.java
+++ src/main/java/com/example/springbootjpa/controller/ReviewController.java
@@ -3,6 +3,7 @@ package com.example.springbootjpa.controller;
 import com.example.springbootjpa.domain.dto.HospitalResponse;
 import com.example.springbootjpa.domain.dto.ReviewResponse;
 import com.example.springbootjpa.service.ReviewService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,12 +15,9 @@ import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/reviews")
+@RequiredArgsConstructor //필요한 생성자를 자동 생성해주는 어노테이션
 public class ReviewController {
     private final ReviewService reviewService;
-
-    public ReviewController(ReviewService reviewService) {
-        this.reviewService = reviewService;
-    }
 
     @GetMapping("") // 리뷰 목록
     public ResponseEntity<List<ReviewResponse>> reviewList(Pageable pageable){
